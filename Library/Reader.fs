b4344e0e51aea51254c7f7db8cda314a120f049c
Adrien Aucher <aucher.adrien@gmail.com>
2021-03-11 17:51:43 +0000

    Library: remove existing Lexer & Parser implementations
    They will be replaced by an implementation that uses FParsec.

--- Library/Reader.fs
+++ Library/Reader.fs
@@ -5,6 +5,6 @@ open System
 module Reader =
 
     let Read (input : string) : Result<Lisp.Sexpr, string> =
-        input |> Lexer.Lex |> Parser.Parse |> function
+        input |> Parser.parse |> function
             | Ok sexpr -> Ok sexpr
             | Error err -> Error $"can not read: {err}"
--- Tests/ParserTest.fs
+++ Tests/ParserTest.fs
@@ -8,192 +8,102 @@ open Library
 let tests =
     testList "Parser" [
         testCase "it should return nil" <| fun _ ->
-            let inp = [
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = ""
+            let got = Parser.parse input
             let want = Lisp.Atom Lisp.Nil
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse number" <| fun _ ->
-            let inp = [
-                Lexer.Number 100
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "100"
+            let got = Parser.parse input
             let want = Lisp.Atom (Lisp.Number 100)
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse symbol" <| fun _ ->
-            let inp = [
-                Lexer.Symbol "symbol"
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "symbol"
+            let got = Parser.parse input
             let want = Lisp.Atom (Lisp.Symbol "symbol")
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse nil symbol as nil" <| fun _ ->
-            let inp = [
-                Lexer.Symbol "nil"
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "nil"
+            let got = Parser.parse input
             let want = Lisp.Atom Lisp.Nil
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse empty list as nil" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "()"
+            let got = Parser.parse input
             let want = Lisp.Atom Lisp.Nil
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse pair" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "symbol1"
-                Lexer.Symbol "."
-                Lexer.Symbol "symbol2"
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(symbol1 . symbol2)"
+            let got = Parser.parse input
             let want = Lisp.Pair ((Lisp.Atom (Lisp.Symbol "symbol1")),
                                   (Lisp.Atom (Lisp.Symbol "symbol2")))
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse pair of dots" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "."
-                Lexer.Symbol "."
-                Lexer.Symbol "."
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(. . .)"
+            let got = Parser.parse input
             let want = Lisp.Pair ((Lisp.Atom (Lisp.Symbol ".")),
                                   (Lisp.Atom (Lisp.Symbol ".")))
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse cdr nested pairs" <| fun _ ->
-            let inp = [
-                Lexer.ParenL        // ( sym1 . ( sym2 . ( sym3 ) ) )
-                Lexer.Symbol "sym1"
-                Lexer.Symbol "."
-                Lexer.ParenL        // ( sym2 . ( sym3 ) )
-                Lexer.Symbol "sym2"
-                Lexer.Symbol "."
-                Lexer.ParenL        // ( sym3 )
-                Lexer.Symbol "sym3"
-                Lexer.ParenR
-                Lexer.ParenR
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(sym1 . (sym2 . (sym3)))"
+            let got = Parser.parse input
             let want = Lisp.Pair ((Lisp.Atom (Lisp.Symbol "sym1")),
                                   (Lisp.Pair ((Lisp.Atom (Lisp.Symbol "sym2")),
                                               (Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym3")]))))
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse list" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "sym1"
-                Lexer.Symbol "sym2"
-                Lexer.Symbol "sym3"
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(sym1 sym2 sym3)"
+            let got = Parser.parse input
             let want = Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym1");
                                    Lisp.Atom (Lisp.Symbol "sym2");
                                    Lisp.Atom (Lisp.Symbol "sym3")]
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse car nested list" <| fun _ ->
-            let inp = [
-                Lexer.ParenL        // ( ( ( sym1 ) sym2 ) sym3 )
-                Lexer.ParenL        // ( ( sym1 ) sym2 )
-                Lexer.ParenL        // ( sym1 )
-                Lexer.Symbol "sym1"
-                Lexer.ParenR
-                Lexer.Symbol "sym2"
-                Lexer.ParenR
-                Lexer.Symbol "sym3"
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(((sym1) sym2) sym3)"
+            let got = Parser.parse input
             let want = Lisp.Sexpr [Lisp.Sexpr [Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym1")];
                                                Lisp.Atom (Lisp.Symbol "sym2")];
                                    Lisp.Atom (Lisp.Symbol "sym3")]
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should parse cadr nested list" <| fun _ ->
-            let inp = [
-                Lexer.ParenL        // ( sym1 ( sym2 ( sym3 ) ) )
-                Lexer.Symbol "sym1"
-                Lexer.ParenL        // ( sym2 ( sym3 ) )
-                Lexer.Symbol "sym2"
-                Lexer.ParenL        // ( sym3 )
-                Lexer.Symbol "sym3"
-                Lexer.ParenR
-                Lexer.ParenR
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(sym1 (sym2 (sym3)))"
+            let got = Parser.parse input
             let want = Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym1");
                             Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym2");
                                         Lisp.Sexpr [Lisp.Atom (Lisp.Symbol "sym3")]]]
             Expect.wantOk "ok" got |> Expect.equal "equality" want
 
         testCase "it should throw unexpected closing parenthesis" <| fun _ ->
-            let inp = [
-                Lexer.ParenR
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = ")"
+            let got = Parser.parse input
             let want = "unexpected closing parenthesis"
             Expect.wantError "error" got |> Expect.equal "equality" want
 
         testCase "it should throw unclosed list error" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "symbol"
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(symbol"
+            let got = Parser.parse input
             let want = "unclosed pair"
             Expect.wantError "error" got |> Expect.equal "equality" want
 
         testCase "it should throw unclosed pair error" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "symbol"
-                Lexer.Symbol "."
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(symbol ."
+            let got = Parser.parse input
             let want = "unclosed pair"
             Expect.wantError "error" got |> Expect.equal "equality" want
 
         testCase "it should throw trailing element in pair" <| fun _ ->
-            let inp = [
-                Lexer.ParenL
-                Lexer.Symbol "sym1"
-                Lexer.Symbol "."
-                Lexer.Symbol "sym2"
-                Lexer.Symbol "sym3"
-                Lexer.EOF
-            ]
-            let got = Parser.Parse (inp)
+            let input = "(sym1 . sym2 sym3"
+            let got = Parser.parse input
             let want = "trailing object in pair"
             Expect.wantError "error" got |> Expect.equal "equality" want
     ]
